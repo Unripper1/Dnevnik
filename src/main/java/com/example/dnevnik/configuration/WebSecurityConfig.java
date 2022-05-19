af9e9bb5fc30f576aab2d101a45d3945f2faacc6
sofia0401 <sofia0401@yandex.ru>
2022-05-19 23:59:29 +0300

    страница входа, регистрации, главная (без навигации)

--- src/main/java/com/example/dnevnik/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/dnevnik/configuration/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/registration", "/home").permitAll()
+                    .antMatchers("/registration", "/hello", "/styles/cssandjs/**", "/js/cssandjs/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
